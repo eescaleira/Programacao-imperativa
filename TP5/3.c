17c4e5a9ba937930a20901adfe1f1af05361f92e
escada <129979364+eescaleira@users.noreply.github.com>
2023-11-07 12:36:56 +0000

    done

--- TP5/3.c
+++ TP5/3.c
@@ -3,12 +3,15 @@ int main()
 {
 	int n, i = 0, m = 0, p = 0, moda = 0;
 	float media = 0;
+
 	printf("Quantos alunos? ");
 	scanf("%i", &n);
+	
 	int notas[n], hist[21];
-	printf("Introduza as notas:\n");
 	for(i = 0; i < 21; i++)
 		hist[i] = 0;
+	
+	printf("Introduza as notas:\n");
 	for(i = 0;i < n;)
 	{
 		scanf("%i", &notas[i]);
@@ -20,6 +23,7 @@ int main()
 			i++;
 		}
 	}
+
 	printf("Notas introduzidas:\n");
 	for(i = 0; i < n; i++)
 		printf("%i: %i\n", i, notas[i]);
@@ -38,6 +42,7 @@ int main()
 	for(i = 0; m == 0 && i < 20;i++)
 		if(m == 0 && hist[20 - i] != 0)
 			m = 20 - i;
+
 	printf("\nMelhor nota: %i\nPior nota: %i\n", m, p);
 
 	/*  */
@@ -45,6 +50,7 @@ int main()
 	for(i = 0; i < 21; i++)
 		media += hist[i] * i;
 	media /= n;
+
 	printf("Media: %.2f\n", media);
 
 	/*  */
@@ -52,6 +58,7 @@ int main()
 	for(i = 0, m = 0; i < 21; i++)
 		if(hist[i] > m)
 			m = hist[i], moda = i;
+	
 	printf("Moda: %i\n", moda);
 	return 0;
 }
