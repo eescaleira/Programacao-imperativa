ed620e29605b684f678334f15ce9c04862dd8fcd
42stuff <129979364+eescaleira@users.noreply.github.com>
2023-11-19 17:09:28 +0000

    Done

--- TP6/2.c
+++ TP6/2.c
@@ -1,5 +1,19 @@
-#include<stdio.h>
-int main()//pointers!!
-{
 
+#include<stdio.h>
+
+void func2(int i, int j, int *res, int *quo)
+{
+	// return (i *j); 1 passo  (return value precisa ser int)
+
+	*res = i * j;
+	*quo = i / j;
 }
+
+int main()
+{
+	int res;
+	int quo;
+	func2(5, 2, &res, &quo);
+	printf("Produto: %i\nQuociente:%i", res, quo);
+	return 0;
+} 
