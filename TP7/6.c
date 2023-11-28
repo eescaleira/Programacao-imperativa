ba23f697eb4a28cb3ef7eeabc463d3cc5347186d
escada <129979364+eescaleira@users.noreply.github.com>
2023-11-28 12:22:59 +0000

    Done

--- TP7/6.c
+++ TP7/6.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 void copia_nao_nulos(int *a1, int *a2, int *dest)
 {
-	int index = 0;
+	int index = 0, *temp = dest;
 	for( ; a1 != NULL; a1++)
 		if(a1 != 0)
 		{
-			dest = a1;
-			dest++;
+			temp = a1;
+			temp++;
 		}	
 	for( ; a2 != NULL; a2++)
 		if(a2 != 0)
 		{
-			dest = a2;
-			dest++;
+			temp = a2;
+			temp++;
 		}
+		temp = NULL;
 }
 
 int main()
